f0edf603cfd21503b11943f63485d85e23f97dcc
Jesús Valera Reales <jesus1902@outlook.com>
2020-03-24 00:33:03 +0100

    Create command registry

--- src/App.php
+++ src/App.php
@@ -4,25 +4,50 @@ declare(strict_types=1);
 
 namespace JesusValera\Minicli;
 
+use Closure;
 use JesusValera\Minicli\IO\CliPrinter;
 
 final class App
 {
     private CliPrinter $printer;
 
+    /** @var array */
+    private $registry = [];
+
     public function __construct(CliPrinter $printer)
     {
         $this->printer = $printer;
     }
 
+    public function getPrinter(): CliPrinter
+    {
+        return $this->printer;
+    }
+
+    public function registerCommand(string $name, Closure $callable): void
+    {
+        $this->registry[$name] = $callable;
+    }
+
+    public function getCommand(string $command): ?Closure
+    {
+        return $this->registry[$command] ?? null;
+    }
+
     public function runCommand(array $argv = []): void
     {
-        $name = 'World';
+        $commandName = 'World';
 
         if (isset($argv[1])) {
-            $name = $argv[1];
+            $commandName = $argv[1];
         }
 
-        $this->printer->display("Hello {$name}");
+        $command = $this->getCommand($commandName);
+        if ($command === null) {
+            $this->printer->display("ERROR: Command \"$commandName\" not found.");
+            exit;
+        }
+
+        $command($argv);
     }
 }
